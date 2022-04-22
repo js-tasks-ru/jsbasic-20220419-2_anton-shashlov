e129bb594f326c64de3c0140e8bc409933657119
Anton Shashlov <9106543@gmail.com>
2022-04-22 18:00:48 +0300

    1-module/5-task (Anton Shahlov)

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,5 @@
+//Anton Shashlov
 function truncate(str, maxlength) {
-  // ваш код...
-}
+  if (str.length < 20) return `${str.slice(0, --maxlength)}`;
+  else return `${str.slice(0, --maxlength)}${"…"}`;
+}  
