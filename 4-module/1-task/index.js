ccd6b0aac4ce890c6f35c0fa25e45812c163ed45
Anton Shashlov <9106543@gmail.com>
2022-05-09 20:01:49 +0300

    4-module-1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
+//Anton Shashlov
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  friends.forEach(el => {
+    let li = document.createElement('li');
+    li.innerHTML = `${el.firstName} ${el.lastName}`;
+    ul.append(li);
+  }); 
+  return ul;
 }
