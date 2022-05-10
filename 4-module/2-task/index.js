73033149385c2281444d27cb52350965a3e6a46c
Anton Shashlov <9106543@gmail.com>
2022-05-10 10:40:25 +0300

    4-module-2-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
+//Anton Shashlov
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = "red";
+  } // ваш код...
 }
