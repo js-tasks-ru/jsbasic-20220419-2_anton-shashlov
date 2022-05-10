b38bd03b6aa298b0694cf897a6915d7bffee2e2d
Anton Shashlov <9106543@gmail.com>
2022-05-10 16:31:17 +0300

    4-module-3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+
+    if (table.rows[i].cells[3].dataset.available === 'true') {
+      table.rows[i].classList.toggle('available'); 
+    }
+    if (table.rows[i].cells[3].dataset.available === 'false') {
+      table.rows[i].classList.toggle('unavailable'); 
+    } else {
+      table.rows[i].setAttribute('hidden', null);
+    }   
+  }     
+  for (let i = 1; i < table.rows.length; i++) {
+    
+    if (table.rows[i].cells[2].innerHTML === 'm') {
+      table.rows[i].classList.add('male');
+    }
+    if (table.rows[i].cells[2].innerHTML === 'f') {
+      table.rows[i].classList.add('female');
+    }
+  } 
+  for (let i = 1; i < table.rows.length; i++) {  
+    if (table.rows[i].cells[1].innerHTML < 18) {
+      table.rows[i].style = "text-decoration: line-through"; 
+    }
+  }  
 }
