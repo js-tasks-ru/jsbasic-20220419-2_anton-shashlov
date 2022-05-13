e863fd938ab3c6ff1fe917858b7b39bcc54fb660
Anton Shashlov <9106543@gmail.com>
2022-05-13 14:16:19 +0300

    5-module-2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let buttonHidden = document.body.querySelector('button');
+  let text = document.getElementById('text');
+  buttonHidden.onclick = () => 
+    (text.hidden === false) 
+      ? text.hidden = true 
+      : text.hidden = false;
 }
